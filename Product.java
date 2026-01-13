ba1b372f0914f04cb6f9849a5872a32f9404b4a5
Deawsa <khophol.phonchanintra@gmail.com>
2026-01-13 13:34:13 +0700

    Lab Sheet 07

---
+++ Product.java
@@ -0,0 +1,54 @@
+import javax.swing.*;
+
+public class Product {
+
+    public String name;
+    public double price = 0;
+    public int quantity = 0;
+
+
+    public void addStock(int amount){
+        if(amount > 0){
+
+            quantity += amount;
+            System.out.println("\n\nAdded " + amount +" item to stock");
+
+        }
+
+
+    }
+    public void sell(int amount){
+
+        if(amount <= quantity){
+
+            quantity-=amount;
+            System.out.println("Sold " + amount +" item");
+
+        }else {
+            System.out.println("Not enough item in stock...");
+
+        }
+
+
+    }
+    public void changePrice(double newPrice){
+
+        if(newPrice > 0){
+            price = newPrice;
+            System.out.println("\n\nChange price to "+newPrice+" baht");
+        }
+    }
+    public double stockValue(){
+
+        return quantity*price;
+
+    }
+    public void showInfo(){
+        System.out.println("Name  : "+name+
+                           "\nPrice : "+price+" Baht/Item"+
+                           "\nStock : "+quantity+" Items"+
+                           "\nStock Value: "+stockValue()+" Baht");
+
+
+    }
+}
